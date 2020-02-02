d0c820bf4b4f47782920693efe2088c136aa51fe
Jonh <hernandez.guille.jon@gmail.com>
2020-02-01 20:31:28 -0700

    something

---
+++ src/components/order/reviewProducts.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import ReviewProduct from './reviewProduct';
+
+class ReviewProducts extends Component {
+    render(){
+        const { className } = this.props;
+        return(
+            <div className={`${className} review-products`}>
+                <ReviewProduct/>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return {cartProducts }
+}
+
+ReviewProducts = connect(mapStateToProps)(ReviewProducts);
+
+export default ReviewProducts;
+/*{
+                    this.props.cartProducts.map(cartProduct => {
+                        return <ReviewProduct
+                        key={cartProduct._id}
+                        {...cartProduct}
+                        />
+                    })
+                }*/
